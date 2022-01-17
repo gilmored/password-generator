0ec9b6cb6dfd3a68a780f712efe164045b8e2189
gilmored <21026373+gilmored@users.noreply.github.com>
2022-01-16 23:53:59 -0600

    password length function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,23 @@ var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'
 var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 var specialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '[', ']', '{', '}', '|', '?', '/', '<', '>'];
 
-
+  function passwordLength() {
+    var length = 0;
+  }
   
+  while (length > 128 || length < 8 || isNaN(length) === true) {
+    length = parseInt(window.prompt("How many characters would you like your password to be?"));
+
+    //Password must be with 8 to 128 characters 
+    if (length < 8) {
+      window.alert("Your password length must be at least 8 characters.");
+    } else if (length > 128) {
+      window.alert("Your password length must be no more than 128 characters.");
+    } else if (isNaN(length) === true){
+      window.alert("Must provide a numeric value.");
+    }
+  }    
+
 
   // user selects if they want lowercase letters
   var lowerChar = window.confirm("Do you want to use lowercase characters?");
@@ -19,13 +34,13 @@ var specialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '[', ']', '
     password += upperCase;
   };
 
-// user selects if they want numbers
+  // user selects if they want numbers
   var numbersChar = window.confirm("Do you want to use numbers?");
   if (numbersChar) {
     password += numbers;
   };
 
-// user selects if they want special characters
+  // user selects if they want special characters
   var specialChar = window.confirm("Do you want to use special characters?");
   if (specialChar) {
     password += specialChar;
